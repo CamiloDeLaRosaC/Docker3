cd1c5d1a43711b15fdedaf3cd146dd8724a84457
dygeraldino <dygeraldino@uninorte.edu.co>
2025-03-07 10:45:44 -0500

    fix .cpp and .rb

--- sample.rb
+++ sample.rb
@@ -1,25 +1,25 @@
 #!/usr/bin/env ruby
 
 def catalan(n)
-    cat = Array.new(n+1, 0)
-    cat[0] = 1
-    (1..n).each do |i|
-      (0...i).each do |j|
-        cat[i] += cat[j] * cat[i-j-1]
-      end
+  cat = Array.new(n+1, 0)
+  cat[0] = 1
+  (1..n).each do |i|
+    (0...i).each do |j|
+      cat[i] += cat[j] * cat[i - j - 1]
     end
-    cat[n]
   end
-  
-  def main
-    N = 30
-    start_time = Time.now
-    (0...N).each do |i|
-      puts "C(#{i}) = #{catalan(i)}"
-    end
-    end_time = Time.now
-    elapsed_ms = (end_time - start_time) * 1000
-    puts "Tiempo de ejecución (en Ruby): %.2f ms" % elapsed_ms
+  cat[n]
+end
+
+def main
+  n = 30  # Variable local
+  start_time = Time.now
+  (0...n).each do |i|
+    puts "C(#{i}) = #{catalan(i)}"
   end
-  
-  main
+  end_time = Time.now
+  elapsed_ms = (end_time - start_time) * 1000
+  puts "Tiempo de ejecución (en Ruby): %.2f ms" % elapsed_ms
+end
+
+main
